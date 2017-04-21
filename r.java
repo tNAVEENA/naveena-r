e0b5ef70fa2adf575d45edbf5ad132cd20d060eb
tNAVEENA <naveenatg2@gmail.com>
2017-04-21 15:47:28 +0530

    Add files via upload

---
+++ r.java
@@ -0,0 +1,20 @@
+package s;
+
+import java.util.Scanner;
+
+public class r {
+	protected int h=0;
+public static void main(String[] args) {
+//	Scanner in = new Scanner(System.in);
+   // String S = in.next();
+//    int start = in.nextInt();
+//    int end = in.nextInt();
+//    System.out.println(S.substring(start,end));
+	char c='\u0000';
+}
+}
+class s{
+public void ma{
+	h=10;
+}
+}
