b8f4deef95293e6e26cf3ea8d5d9bc5c44655d60
tNAVEENA <naveenatg2@gmail.com>
2017-07-05 17:21:02 +0530

    Add files via upload

---
+++ Power.java
@@ -0,0 +1,30 @@
+package naa;
+
+public class Power {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=5,p=3,result=1;
+		 
+	    if(n>=0&&p==0)
+	     {
+	        result=1;
+	     }
+	    else if(n==0&&p>=1)
+	      { 
+	         result=0;
+	      }
+	    else
+	     {
+	         for(int i=1;i<=p;i++)
+		 {
+	            result=result*n;
+	 	 }	    
+	     }
+	    System.out.println(n+"^"+p+"="+result);
+		
+	  }
+	
+	}
+
+
