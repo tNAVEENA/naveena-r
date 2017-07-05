f076928eaf8793f3ddc71bf241b06628f8c96f2d
tNAVEENA <naveenatg2@gmail.com>
2017-07-05 17:17:12 +0530

    Add files via upload

---
+++ Count.java
@@ -0,0 +1,31 @@
+package naa;
+
+import java.util.Scanner;
+
+public class Count {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int no,a=0;
+		 Scanner s=new Scanner(System.in);
+		System.out.println("Enter any number : ");
+		 no = s.nextInt();
+
+		if(no<0)
+		{
+		no=no * -1;
+		}
+		else if(no==0)
+		{
+		no=1;
+		}
+		while(no>0)
+		{
+		no=no/10;
+		a++;
+		}
+		System.out.println("Number of Digits in given number is: "+a);
+		}
+	}
+
+
