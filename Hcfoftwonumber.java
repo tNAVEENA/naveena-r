cb1c09c7f3dca893c01b53c3ff95a774a9dd7bee
tNAVEENA <naveenatg2@gmail.com>
2017-07-06 17:18:22 +0530

    Add files via upload

---
+++ Hcfoftwonumber.java
@@ -0,0 +1,31 @@
+package guvi;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Hcfoftwonumber {
+
+		// TODO Auto-generated constructor stub
+		 public static void main(String args[])throws IOException
+	        {
+	            BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	            System.out.print("Enter the First no : ");
+	            int n1=Integer.parseInt(br.readLine());
+	            System.out.print("Enter the Second no : ");
+	            int n2=Integer.parseInt(br.readLine());
+	            int hcf=0;
+	            int min = Math.min(n1,n2);
+	             
+	            for(int i=min; i >= 1; i--)
+	            {
+	                if(n1%i == 0 && n2%i == 0)
+	                {
+	                    hcf = i;
+	                    break;
+	                }
+	            }        
+	            System.out.print("\nThe hcf of "+n1+" and "+n2+" = "+hcf);
+	}
+
+}
