555d2545e58fa6134cb251a682340232b3bdb7b6
tNAVEENA <naveenatg2@gmail.com>
2017-07-06 09:28:18 +0530

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,24 @@
+package guvi;
+
+public class Palindrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int r,sum=0,temp;    
+		  int n=454; 
+		  
+		  temp=n;    
+		  while(n>0){    
+		   r=n%10;    
+		   sum=(sum*10)+r;    
+		   n=n/10;    
+		  }    
+		  if(temp==sum)    
+		   System.out.println("palindrome number ");    
+		  else    
+		   System.out.println("not palindrome");    
+		}  
+		
+	}
+
+
