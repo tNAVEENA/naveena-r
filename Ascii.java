e3cb76fec92dc7baf8a5a1a3dfb33b14323963d5
tNAVEENA <naveenatg2@gmail.com>
2017-07-06 17:12:50 +0530

    Add files via upload

---
+++ Ascii.java
@@ -0,0 +1,21 @@
+package guvi;
+
+public class Ascii {
+
+		
+	// TODO Auto-generated constructor stub
+		 public static void main(String args[])
+		    {
+			
+		        String ch;
+		        int i;
+		        for(i=1; i<=255; i++)
+		        {
+		            ch = new Character((char)i).toString();
+		            System.out.print(i+ " -> " + ch + "\t");
+		        }
+				
+		    }
+	}
+
+
