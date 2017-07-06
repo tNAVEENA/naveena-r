1d7752040d7cd6590c1d0de9016497e88b1ae6e0
tNAVEENA <naveenatg2@gmail.com>
2017-07-06 10:05:37 +0530

    Add files via upload

---
+++ Printmultiplicationtable.java
@@ -0,0 +1,19 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class Printmultiplicationtable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+        Scanner s = new Scanner(System.in);
+	System.out.print("Enter number:");        
+	int n=s.nextInt();
+        for(int i=1; i <= 10; i++)
+        {
+            System.out.println(n+" * "+i+" = "+n*i);
+        }
+	}
+
+}
