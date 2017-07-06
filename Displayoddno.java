ef51635196583f162f9276de0f26c3c9a43dd3a2
tNAVEENA <naveenatg2@gmail.com>
2017-07-06 09:39:08 +0530

    Add files via upload

---
+++ Displayoddno.java
@@ -0,0 +1,16 @@
+package guvi;
+
+public class Displayoddno {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		for(i=1;i<=100;i++){//loop for take 1 to 100 numbers
+		if(i%2!=0)//condition to find odd numbers between 1 to 100
+		{
+		System.out.println(i);
+	}
+
+}
+}
+}
