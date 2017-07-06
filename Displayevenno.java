49f3d952f085a1ac0fc5665aa6ba59457fef7fce
tNAVEENA <naveenatg2@gmail.com>
2017-07-06 09:42:57 +0530

    Add files via upload

---
+++ Displayevenno.java
@@ -0,0 +1,20 @@
+package guvi;
+
+public class Displayevenno {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		   int limit = 50;
+           
+           System.out.println("Printing Even numbers between 1 and " + limit);
+          
+           for(int i=1; i <= limit; i++){
+                  
+                   // if the number is divisible by 2 then it is even
+                   if( i % 2 == 0){
+                           System.out.print(i + " ");
+	}
+
+}
+}
+}
